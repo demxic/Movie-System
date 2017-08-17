5f81c47dfdfa3954a8d07af14374a879e778fda1
Xicoténcatl Delgado Maya <demxic@gmail.com>
2017-08-17 17:04:45 -0500

    feat : Add the save_to_file method in the user class (CSV format)

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from movie import Movie
 from user import User
 
 my_movie = Movie('The Matrix', 'Sci-Fi', True)
-print(my_movie.name)
+
 user = User('Xico')
-print(user)
-user.movies.append(my_movie)
-print(user.watched_movies())
+user.add_movie('The Matrix', 'Sci-Fi', False)
+user.add_movie('No sé', 'comedia', False)
+user.save_to_file()
--- user.py
+++ user.py
@@ -9,11 +9,17 @@ class User(object):
     def __repr__(self):
         return "<User {}>".format(self.name)
 
-    def add_movie(self, name, genre):
-        self.append(Movie(name, genre, True))
+    def add_movie(self, name, genre, watched):
+        self.movies.append(Movie(name, genre, True))
 
     def delete_movie(self, name):
-        filter(lambda movie: movie.name != name, self.movies)
+        return list(filter(lambda movie: movie.name != name, self.movies))
+
+    def save_to_file(self):
+        with open ("{}.txt".format(self.name), 'w') as f:
+            f.write(self.name+'\n')
+            for movie in self.movies:
+                f.write('{},{},{}'.format(movie.name, movie.genre, movie.watched)+'\n')
 
     def watched_movies(self):
         return list(filter(lambda movie: movie.watched, self.movies))
