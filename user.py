9b230e441d29e853a59fd0436622330dc9278cdf
Xicoténcatl Delgado Maya <demxic@gmail.com>
2017-08-17 16:38:06 -0500

    feat : Add the delete_movie method to the User class

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+from movie import Movie
+
+
 class User(object):
     def __init__(self, name):
         self.name = name
@@ -6,5 +9,11 @@ class User(object):
     def __repr__(self):
         return "<User {}>".format(self.name)
 
+    def add_movie(self, name, genre):
+        self.append(Movie(name, genre, True))
+
+    def delete_movie(self, name):
+        filter(lambda movie: movie.name != name, self.movies)
+
     def watched_movies(self):
-        return list(filter(lambda movie : movie.watched, self.movies))
+        return list(filter(lambda movie: movie.watched, self.movies))
