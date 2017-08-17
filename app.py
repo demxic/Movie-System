6cb6d0880d3b72122092938a18420111f40cdb19
Xicoténcatl Delgado Maya <demxic@gmail.com>
2017-08-17 13:40:31 -0500

    feat: Add the User class

---
+++ app.py
@@ -0,0 +1,6 @@
+from movie import Movie
+from user import User
+
+my_movie = Movie('The Matrix', 'Sci-Fi')
+print(my_movie.name)
+user = User('Xico')
---
+++ movie.py
@@ -0,0 +1,6 @@
+class Movie(object):
+
+    def __init__(self, name, genre):
+        self.name = name
+        self.genre = genre
+        self.director
